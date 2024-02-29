83406cc1085b0cce0eb4027bc9c2e5912bce7baa
kxd1115 <kxd18086507240@163.com>
2024-02-29 22:52:18 +0800

    js小练习

---
+++ web_study_on_ubuntu/study_20240229/practice_students.js
@@ -0,0 +1,22 @@
+let students = [
+  { name: "Susan", ID: 1, score: "90", rank: 6 },
+  { name: "Jackson", ID: 2, score: "88", rank: 7 },
+  { name: "Bob", ID: 3, score: "45", rank: 18 },
+  { name: "Jennie", ID: 3, score: "99", rank: 1 },
+  { name: "Amy", ID: 3, score: "39", rank: 21 },
+  { name: "Lisa", ID: 3, score: "78", rank: 8 },
+];
+
+let studentsFullInfo = students.map((val) => `${val.name}: 学号${val.ID}总分${val.score}排名第${val.rank}`);
+// console.log(studentsFullInfo);
+
+let failList = students.filter((val) => val.score < 60);
+// console.log(failList);
+
+// 自己实现一个foreach()方法
+// 待完成
+function myForEach(callback, thisArg) {
+  return function callback() {
+
+  }
+}
