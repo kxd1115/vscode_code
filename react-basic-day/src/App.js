3a97de903b70da08d1640a754a5d6fb7258a2641
kxd1115 <kxd18086507240@163.com>
2024-03-10 22:09:58 +0800

    关于父子通信

--- react-basic-day/src/App.js
+++ react-basic-day/src/App.js
@@ -1,11 +1,21 @@
 import { useState, useRef } from 'react'
 
+// 父组件传递数据
+// 子组件接受数据，使用props.name接收
+
+function Son(props) {
+  return (
+    <div>
+      this is son, {props.name}
+    </div>
+  )
+}
 
 function App() {
-
+  const name = 'this is app name';
   return (
     <div className="App">
-      test
+      <Son name={name} />
     </div>
   );
 }
