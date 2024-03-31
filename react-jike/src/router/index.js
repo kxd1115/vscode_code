9638ec09417c991e8cff0f171c8ef4290d7c2a0a
kxd1115 <kxd18086507240@163.com>
2024-03-31 23:23:54 +0800

    二级路由配置

--- react-jike/src/router/index.js
+++ react-jike/src/router/index.js
@@ -4,12 +4,30 @@ import Layout from "@/pages/Layout";
 
 import { createBrowserRouter } from 'react-router-dom';
 import { AuthRoute } from "@/components/AuthRoute";
+import Article from "@/pages/Article";
+import Home from "@/pages/Home";
+import Publish from "@/pages/Publish";
 
 const router = createBrowserRouter([
   {
     path: '/',
     // 测试有无token条件下的高阶组件是否能够跳转
-    element: <AuthRoute> <Layout /></AuthRoute>
+    element: <AuthRoute> <Layout /></AuthRoute>,
+    children: [
+      {
+        path: '/home',
+        index: true, // 设置为默认二级路由
+        element: <Home></Home>
+      },
+      {
+        path: '/article',
+        element: <Article></Article>
+      },
+      {
+        path: '/publish',
+        element: <Publish></Publish>
+      },
+    ]
   },
   { 
     path: '/login',
