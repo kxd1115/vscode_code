42a67c28121bb9ece670a1402cba69447280b490
kxd1115 <kxd18086507240@163.com>
2024-04-01 22:23:16 +0800

    Layout-根据路由路径高亮菜单

--- react-jike/src/pages/Layout/index.js
+++ react-jike/src/pages/Layout/index.js
@@ -6,7 +6,7 @@ import {
   LogoutOutlined,
 } from '@ant-design/icons'
 import './index.scss'
-import { Outlet, useNavigate } from 'react-router-dom'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 
 const { Header, Sider } = Layout
 
@@ -32,8 +32,15 @@ const items = [
 const GeekLayout = () => {
   const navigate  = useNavigate()
   const onMenuClick = (route) => {
+    console.log("菜单被点击了", route);
     navigate(route.key); // 跳转至对应路由
   }
+
+  // 反向高亮
+  // 1. 获取当前路由路径
+  const location = useLocation();
+  const selectedKey = location.pathname;
+
   return (
     <Layout>
       <Header className="header">
@@ -52,7 +59,8 @@ const GeekLayout = () => {
           <Menu
             mode="inline"
             theme="dark"
-            defaultSelectedKeys={['1']}
+            // defaultSelectedKeys={['1']}
+            selectedKeys={selectedKey}
             onClick={onMenuClick}
             items={items}
             style={{ height: '100%', borderRight: 0 }}></Menu>
