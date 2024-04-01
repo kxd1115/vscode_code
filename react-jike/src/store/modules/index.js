985f9b948c18d071257ee348caaa929bdbcfe0cd
kxd1115 <kxd18086507240@163.com>
2024-04-01 22:37:08 +0800

    Layout-获取用户信息

--- react-jike/src/store/modules/index.js
+++ react-jike/src/store/modules/index.js
@@ -6,20 +6,24 @@ const userStore = createSlice({
   name: "user",
   // 数据初始状态
   initialState: {
-    token: getToken() || ''
+    token: getToken() || '',
+    userInfo: {},
   },
   // 同步修改方法
   reducers: {
     setToken(state, action) {
-      state.token = action.payload
+      state.token = action.payload;
       // localStorage存一份
       _setToken(action.payload)
+    },
+    setUserInfo(state, action) {
+      state.userInfo = action.payload;
     }
   }
 });
 
 // 结构actionCreater
-const { setToken } = userStore.actions;
+const { setToken, setUserInfo } = userStore.actions;
 
 // 获取reducer函数
 const userReducer = userStore.reducer;
@@ -34,6 +38,16 @@ const fetchLogin = (loginForm) => {
   }
 };
 
+// 异步方法 获取个人用户信息
+const fetchUserInfo = () => {
+  return async (dispatch) => {
+    // 发送异步请求，获取个人信息
+    const res = await request.get('/user/profile');
+    // 提交同步action进行token的存入
+    dispatch(setUserInfo(res.data));
+  }
+};
+
 // 导出
-export { setToken, fetchLogin };
+export { setToken, fetchLogin, fetchUserInfo };
 export default userReducer;
