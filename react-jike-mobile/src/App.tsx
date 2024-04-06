9c8b3c013e35466ae3ed3e4b6e974a93b35969d9
kxd1115 <kxd18086507240@163.com>
2024-04-06 14:38:17 +0800

    引入ant Design mobile

--- react-jike-mobile/src/App.tsx
+++ react-jike-mobile/src/App.tsx
@@ -1,9 +1,11 @@
 
-function App() {
+import { Button } from "antd-mobile";
 
+function App() {
   return (
     <>
       this is App
+      <Button color="success">click me!</Button>
     </>
   )
 }
