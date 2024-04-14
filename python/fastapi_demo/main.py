8f3572cef43b7c3f0306fec413006f91aae44452
kxd1115 <kxd18086507240@163.com>
2024-04-14 23:10:07 +0800

    通过ORM操作数据库，获取接口

--- python/fastapi_demo/main.py
+++ python/fastapi_demo/main.py
@@ -16,6 +16,7 @@ from routers.select import select
 from routers.tables import table
 from routers.user01 import user1
 from routers.user02 import user2
+from routers.student import studentAPI
 
 
 app = FastAPI()
@@ -30,13 +31,15 @@ async def run():
 
 app.mount("/statics", StaticFiles(directory="statics"))
 
-app.include_router(user1, tags=["请求参数1"])
-app.include_router(user2, tags=["请求参数2"])
-app.include_router(select, tags=["查询参数01"])
-app.include_router(datas, tags=["查询参数02"])
+# app.include_router(user1, tags=["请求参数1"])
+# app.include_router(user2, tags=["请求参数2"])
+# app.include_router(select, tags=["查询参数01"])
+# app.include_router(datas, tags=["查询参数02"])
 
-app.include_router(datas, tags=['form表单'])
-app.include_router(table, tags=['table数据'])
-app.include_router(files, tags=['文件'])
-app.include_router(request, tags=['请求头内容'])
-app.include_router(response01, tags=['响应模型'])
+# app.include_router(datas, tags=['form表单'])
+# app.include_router(table, tags=['table数据'])
+# app.include_router(files, tags=['文件'])
+# app.include_router(request, tags=['请求头内容'])
+# app.include_router(response01, tags=['响应模型'])
+
+app.include_router(studentAPI, prefix='/student', tags=['选课系统学生接口'])
---
+++ python/fastapi_demo/routers/student.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from database.models import Student
+
+studentAPI = APIRouter()
+
+
+@studentAPI.get("/")
+async def get_students():
+    # (1) 查询所有学生 all()
+    # students = await Student.all() # 返回一个Queryset列表对象，必须使用异步函数才能够遍历内容
+    
+    # (2) 过滤查询 filter()  # 返回Queryset
+    
+    # (3) 查询单个学生 get()  # 返回单个对象
+    
+    # (4) 模糊查询 filter(clas_id__gt=2) # 返回Queryset
+    # gt 大于  gte 大于等于
+    # students = await Student.filter(clas_id__gt=2)
+    # students = await Student.filter(clas_id__range=[1,3]) # 查询1-3班的学生
+    # students = await Student.filter(clas_id__in=[1,2]) # 查询1班和2班的学生
+    
+    # (5) values查询
+    # students = await Student.filter(clas_id__in=[1,2]).values('id', 'name', 'clas_id') # 查询1班和2班的学生
+    # 通过values方法，返回一个由对象组成的列表, 包含对应列出的key
+    
+    students = await Student.all().values('id', 'name', 'clas_id')
+    return {
+      "操作": students
+    }
+
+@studentAPI.get("/{student_id}")
+async def get_student(student_id: int):
+    return {
+      "操作": f"查看id={student_id}的学生"
+    }
+
+@studentAPI.post("/{student_id}")
+async def add_student(student_id: int):
+    return {
+      "操作": "添加id={student_id}的学生"
+    }
+
+@studentAPI.put("/{student_id}")
+async def put_student(student_id: int):
+    return {
+      "操作": "更新id={student_id}的学生"
+    }
+
+@studentAPI.delete("/{student_id}")
+async def del_student(student_id: int):
+    return {
+      "操作": "删除id={student_id}的学生"
+    }
